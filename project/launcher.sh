0a81a89a6f61ba5271b272e3743cbf08dd38fb9c
FANG-MING <2635540050@qq.com>
2023-02-19 20:22:51 +0800

    'init'

--- project/launcher.sh
+++ project/launcher.sh
@@ -4,4 +4,4 @@ WORKDIR=$2
 
 shift 2
 # export CUDA_VISIBLE_DEVICES=0
-python train_point.py --py-config $CONFIG --work-dir $WORKDIR "$@"
+python train.py --py-config $CONFIG --work-dir $WORKDIR "$@"
